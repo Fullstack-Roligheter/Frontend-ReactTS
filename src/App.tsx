55117107bd91d51fab7cadf81fba7b370953850b
Emil <emil.johansson@studerande-skys.se>
2022-06-13 14:39:00 +0200

    Added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import "./App.css";
 import Button from "@mui/material/Button";
 import ExpenseRoutes from "./Routes";
+import Header from "./shared/Layout";
 
 function App() {
-  return <div>{ExpenseRoutes}</div>;
+  return (
+    <div>
+      <Header/>
+      <div>{ExpenseRoutes}</div>
+    </div>
+    ) ;
 };
 
 export default App;
