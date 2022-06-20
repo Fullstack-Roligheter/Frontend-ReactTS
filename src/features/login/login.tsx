a2a4fe53c330db59344830c180245ea75cc06371
Daniela Escobar <daniela.b.escobar@gmail.com>
2022-06-20 13:54:57 +0200

    Fix Login form

--- src/features/login/login.tsx
+++ src/features/login/login.tsx
@@ -1,13 +1,69 @@
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { InputBase } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import SubmitButton from '../../shared/buttons/button-default';
+
+
+
+
 const LogIn = () => {
+
+	const [showPassword, setShowPassword] = useState(false);
+	const handleClickShowPassword = () => setShowPassword(!showPassword);
+	const handleMouseDownPassword = () => setShowPassword(!showPassword);
+	
 	return (
-		<>
-			<div>
-				<h1>Login sidan</h1>
-			</div>
-			<div>
-				<p>Woop Woop</p>
-			</div>
-		</>
-	)
+		<Grid
+  container
+  spacing={1}
+  direction="column"
+  alignItems="center"
+  justifyContent="center"
+  style={{ minHeight: '70vh'}}
+>
+  <Grid item xs={3}
+   alignItems="center">
+	<h3>Logga in</h3>
+	</Grid>
+	<Grid item>
+
+	<form>
+		  <TextField  
+		  label='User name' variant="outlined" type="text"
+		  style ={{width: '100%'}}
+		  />
+  <br/>
+  <br/>
+  <TextField
+  label='Password'
+  variant="outlined"
+  type={showPassword ? "text" : "password"} 
+  
+  InputProps={{ endAdornment: (
+	  <InputAdornment position="end">
+        <IconButton
+          aria-label="toggle password visibility"
+          onClick={handleClickShowPassword}
+          onMouseDown={handleMouseDownPassword}
+		  >
+          {showPassword ? <Visibility /> : <VisibilityOff />}
+        </IconButton>
+      </InputAdornment>
+    )
+}}
+/>
+<br />
+<br />
+<SubmitButton />
+</form>
+</Grid>
+</Grid>   
+		
+	  );
 }
+
 export default LogIn
