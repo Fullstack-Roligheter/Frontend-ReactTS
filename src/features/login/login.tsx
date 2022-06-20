f9d73ae8a05fb9b3747662b7865f6bccc8287624
Lachlan Morrison <lmorrison85@gmail.com>
2022-06-20 22:09:38 +0200

    login fungerar lite

--- src/features/login/login.tsx
+++ src/features/login/login.tsx
@@ -1,69 +1,106 @@
-import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { InputBase } from '@mui/material';
-import Grid from '@mui/material/Grid';
-import IconButton from '@mui/material/IconButton';
-import InputAdornment from '@mui/material/InputAdornment';
-import TextField from '@mui/material/TextField';
-import { useState } from 'react';
-import SubmitButton from '../../shared/buttons/button-default';
+import { useState } from 'react'
+import { useNavigate } from 'react-router'
 
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import Grid from '@mui/material/Grid'
+import IconButton from '@mui/material/IconButton'
+import InputAdornment from '@mui/material/InputAdornment'
+import TextField from '@mui/material/TextField'
 
-
+import SubmitButton from '../../shared/buttons/button-default'
+import { UserLogin } from '../../shared/fetch/user'
 
 const LogIn = () => {
+	const [showPassword, setShowPassword] = useState(false)
+	const handleClickShowPassword = () => setShowPassword(!showPassword)
+	const handleMouseDownPassword = () => setShowPassword(!showPassword)
 
-	const [showPassword, setShowPassword] = useState(false);
-	const handleClickShowPassword = () => setShowPassword(!showPassword);
-	const handleMouseDownPassword = () => setShowPassword(!showPassword);
-	
+	const [formData, setFormData] = useState({
+		userName: '',
+		password: '',
+	})
+
+	const navigate = useNavigate()
+
+	const handleChange = (e: any) => {
+		const { name, value } = e.target
+		setFormData({
+			...formData,
+			[name]: value,
+		})
+	}
+
+	const handleSubmit = (e: any) => {
+		e.preventDefault()
+		UserLogin(formData)
+			.then((response) => {
+				alert('Access Granted')
+				console.log('user id:', response)
+				navigate('/omoss/')
+			})
+			.catch((error) => {
+				console.log('Error:', error)
+				if (error.request.status === 401) {
+					alert('Access Denied')
+				}
+			})
+	}
+
 	return (
 		<Grid
-  container
-  spacing={1}
-  direction="column"
-  alignItems="center"
-  justifyContent="center"
-  style={{ minHeight: '70vh'}}
->
-  <Grid item xs={3}
-   alignItems="center">
-	<h3>Logga in</h3>
-	</Grid>
-	<Grid item>
-
-	<form>
-		  <TextField  
-		  label='User name' variant="outlined" type="text"
-		  style ={{width: '100%'}}
-		  />
-  <br/>
-  <br/>
-  <TextField
-  label='Password'
-  variant="outlined"
-  type={showPassword ? "text" : "password"} 
-  
-  InputProps={{ endAdornment: (
-	  <InputAdornment position="end">
-        <IconButton
-          aria-label="toggle password visibility"
-          onClick={handleClickShowPassword}
-          onMouseDown={handleMouseDownPassword}
-		  >
-          {showPassword ? <Visibility /> : <VisibilityOff />}
-        </IconButton>
-      </InputAdornment>
-    )
-}}
-/>
-<br />
-<br />
-<SubmitButton />
-</form>
-</Grid>
-</Grid>   
-		
-	  );
+			container
+			spacing={1}
+			direction='column'
+			alignItems='center'
+			justifyContent='center'
+			style={{ minHeight: '70vh' }}
+		>
+			<Grid item xs={3} alignItems='center'>
+				<h3>Logga in</h3>
+			</Grid>
+			<Grid item>
+				<form onSubmit={handleSubmit}>
+					<TextField
+						label='User Name'
+						variant='outlined'
+						type='text'
+						name='userName'
+						required={true}
+						value={formData.userName}
+						onChange={handleChange}
+						style={{ width: '100%' }}
+					/>
+					<br />
+					<br />
+					<TextField
+						label='Password'
+						variant='outlined'
+						name='password'
+						onChange={handleChange}
+						value={formData.password}
+						required={true}
+						type={showPassword ? 'text' : 'password'}
+						InputProps={{
+							endAdornment: (
+								<InputAdornment position='end'>
+									<IconButton
+										aria-label='toggle password visibility'
+										onClick={handleClickShowPassword}
+										onMouseDown={handleMouseDownPassword}
+									>
+										{showPassword ? <Visibility /> : <VisibilityOff />}
+									</IconButton>
+								</InputAdornment>
+							),
+						}}
+					/>
+					<br />
+					<br />
+					<SubmitButton />
+				</form>
+			</Grid>
+		</Grid>
+	)
 }
 
 export default LogIn
