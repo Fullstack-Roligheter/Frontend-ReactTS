7fa48c7083e22ce7ab935b6a429d07f293817c2c
Lachlan Morrison <lmorrison85@gmail.com>
2022-06-21 14:57:19 +0200

    Revert "merge frontend-lei till dev"
    This reverts commit b9ad6c1bd72ea779d953ea428c344d27a5e2c3dc, reversing
    changes made to 76f4bb44421e6f30ba454784debb5b89d1394c1e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CreateSaving from "./Component/createSaving";
-import CheckSavingPlans from "./Component/getSavingPlans";
-import AppLayout from "./Component/AppLayout";
-import EditSavingPlan from "./Component/EditSavingPlan";
+import './App.css'
+import ExpenseRoutes from './routes'
 
-const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route path="saving">
-            <Route index element={<CreateSaving />} />
-            <Route path="getplans" element={<CheckSavingPlans />} />
-            <Route path="editplan/:id" element={<EditSavingPlan />} />
-          </Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
+function App() {
+	return <>{ExpenseRoutes}</>
+}
 
-export default App;
+export default App
--- src/TypeMap/model.tsx
+++
@@ -1,8 +0,0 @@
-export interface Plan {
-  savingId: number;
-  name: string;
-  amount: number;
-  planStartDate: string;
-  planEndDate: string;
-  countDown: number;
-}
