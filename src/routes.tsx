1a9314d1e02dc9a5a4cb46e946248842cac13b91
Mikaela Hedström <mikaela.hedstrom@gmail.com>
2022-08-25 15:26:54 +0200

    Fix routes with local storage.

--- src/routes.tsx
+++ src/routes.tsx
@@ -13,36 +13,36 @@ import Layout from './shared/layout/layout'
 import AuthenticatedLayout from './shared/layout/authenticatedLayout'
 
 const AppRouter = () => {
-	let UserIsLoggedIn = localStorage.getItem('user')
-	console.log('routing user is logged in', UserIsLoggedIn)
+  let UserIsLoggedIn = JSON.parse(localStorage.getItem('user') || '""')
+  console.log('routing user is logged in', UserIsLoggedIn)
 
-	return (
-		<BrowserRouter>
-			<Layout>
-				<Routes>
-					<Route>
-						<Route element={<Layout user={UserIsLoggedIn} />}>
-							<Route index element={<WelcomeFeature />} />
-							<Route path='/omoss' element={<OmOss />} />
-							<Route path='/login' element={<LogIn />} />
-							<Route path='/faq' element={<Faq />} />
-							<Route
-								path='/'
-								element={<AuthenticatedLayout user={UserIsLoggedIn} />}
-							>
-								<Route path='/:id' element={<DashboardFeature />} />
-								<Route path='/saving' element={<SavingsLayout />}>
-									<Route index element={<CreateSaving />} />
-									<Route path='getplans' element={<CheckSavingPlans />} />
-									<Route path='editplan/:id' element={<EditSavingPlan />} />
-								</Route>
-							</Route>
-						</Route>
-					</Route>
-				</Routes>
-			</Layout>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route>
+            <Route element={<Layout user={UserIsLoggedIn} />}>
+              <Route index element={<WelcomeFeature />} />
+              <Route path='/omoss' element={<OmOss />} />
+              <Route path='/login' element={<LogIn />} />
+              <Route path='/faq' element={<Faq />} />
+              <Route
+                path='/'
+                element={<AuthenticatedLayout user={UserIsLoggedIn} />}
+              >
+                <Route path='/:id' element={<DashboardFeature />} />
+                <Route path='/saving' element={<SavingsLayout />}>
+                  <Route index element={<CreateSaving />} />
+                  <Route path='getplans' element={<CheckSavingPlans />} />
+                  <Route path='editplan/:id' element={<EditSavingPlan />} />
+                </Route>
+              </Route>
+            </Route>
+          </Route>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
 }
 
 export default AppRouter
