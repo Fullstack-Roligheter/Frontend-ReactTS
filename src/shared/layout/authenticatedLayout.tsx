3626348d5996a2ccce8d690c42d4e05157033a66
MikaelaNystrom <mikaela.hedstrom@gmail.com>
2022-08-29 15:50:41 +0200

    Try to find extra footer

--- src/shared/layout/authenticatedLayout.tsx
+++ src/shared/layout/authenticatedLayout.tsx
@@ -5,30 +5,30 @@ import { Box } from '@mui/material'
 import Sidebar from './sidebar/sidebar'
 
 const AuthenticatedLayout = (props: any) => {
-	console.log('authenticated props:', props)
+  // console.log('authenticated props:', props)
 
-	let UserIsLoggedIn = localStorage.getItem('user')
-	console.log('authenticatedLayout: ', UserIsLoggedIn)
+  let UserIsLoggedIn = localStorage.getItem('user')
+  // console.log('authenticatedLayout: ', UserIsLoggedIn)
 
-	if (UserIsLoggedIn === null) {
-		return <Navigate to='/login' replace />
-	}
+  if (UserIsLoggedIn === null) {
+    return <Navigate to='/login' replace />
+  }
 
-	return (
-		<>
-			<Box
-				sx={{
-					minHeight: 'calc(100vh - 70px)',
-					display: 'flex',
-					marginTop: '70px',
-				}}
-			>
-				<Header />
-				<Sidebar />
-				<Outlet />
-			</Box>
-			<Footer />
-		</>
-	)
+  return (
+    <>
+      <Box
+        sx={{
+          minHeight: 'calc(100vh - 70px)',
+          display: 'flex',
+          marginTop: '70px',
+        }}
+      >
+        <Header />
+        <Sidebar />
+        <Outlet />
+      </Box>
+      {/* <Footer /> */}
+    </>
+  )
 }
 export default AuthenticatedLayout
