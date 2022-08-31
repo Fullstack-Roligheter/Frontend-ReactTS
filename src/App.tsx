808dca55da03f46f68a598c802cc2a5c559d7410
Mikaela Hedström <mikaela.hedstrom@gmail.com>
2022-08-31 08:08:13 +0200

    Kept logged in between routes but not on reload

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,16 @@ import "./App.css";
 import AppRouter from "./routes";
 
 const App: React.FC = () => {
+  window.onbeforeunload = function (e) {
+    window.onunload = function () {
+      window.localStorage.isMySessionActive = "false";
+    }
+    return undefined;
+  };
+  window.onload = function () {
+    window.localStorage.isMySessionActive = "true";
+  };
+
   return <AppRouter />;
 };
 
