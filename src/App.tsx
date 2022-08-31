8523358176be312db3127db0f7b5e44a221ba21a
Mikaela Hedström <mikaela.hedstrom@gmail.com>
2022-08-31 08:13:16 +0200

    Last commit not working as intended.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,6 @@ import "./App.css";
 import AppRouter from "./routes";
 
 const App: React.FC = () => {
-  window.onbeforeunload = function (e) {
-    window.onunload = function () {
-      window.localStorage.isMySessionActive = "false";
-    }
-    return undefined;
-  };
-  window.onload = function () {
-    window.localStorage.isMySessionActive = "true";
-  };
 
   return <AppRouter />;
 };
