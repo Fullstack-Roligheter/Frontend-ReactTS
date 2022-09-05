786cd1c9fb3d3375bf225f0c3679e6bb83e50ffd
Lei <babyonepucca@yahoo.com>
2022-09-05 09:52:55 +0200

    Fixed routing on different screens

--- src/features/savings/SavingsLayout.tsx
+++ src/features/savings/SavingsLayout.tsx
@@ -1,28 +1,28 @@
-import Button from "@mui/material/Button";
-import { NavLink, Outlet } from "react-router-dom";
-import { Fragment } from "react";
+import Button from '@mui/material/Button'
+import { NavLink, Outlet } from 'react-router-dom'
+import { Fragment } from 'react'
 
 const SavingsLayout: React.FC = () => {
   return (
     <Fragment>
-      <div className="container">
-        <NavLink
+      <div className='container'>
+        {/*  <NavLink
           to={"/saving"}
           className={({ isActive }) => `text-link ${isActive ? "active" : ""}`}
         >
           <Button variant="contained">Saving Plan</Button>
-        </NavLink>
+        </NavLink> */}
         <NavLink
-          to={"/saving/getplans"}
-          className={({ isActive }) => `text-link ${isActive ? "active" : ""}`}
+          to={'create'}
+          className={({ isActive }) => `text-link ${isActive ? 'active' : ''}`}
         >
-          <Button variant="contained">Get Saving Plans</Button>
+          <Button variant='contained'>Create saving plan</Button>
         </NavLink>
       </div>
-      <div className="container">
+      <div className='container'>
         <Outlet />
       </div>
     </Fragment>
-  );
-};
-export default SavingsLayout;
+  )
+}
+export default SavingsLayout
