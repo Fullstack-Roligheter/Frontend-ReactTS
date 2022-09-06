99e6b6d8b8732420ef6b9ab67c642c017e93a38c
Mikaela Hedström <mikaela.hedstrom@gmail.com>
2022-09-06 13:29:17 +0200

    Clean code in sidebar

--- src/shared/layout/sidebar/sidebar.tsx
+++ src/shared/layout/sidebar/sidebar.tsx
@@ -1,10 +1,8 @@
-import * as React from 'react'
 import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
 import CssBaseline from '@mui/material/CssBaseline'
 import Toolbar from '@mui/material/Toolbar'
 import List from '@mui/material/List'
-import Typography from '@mui/material/Typography'
 import Divider from '@mui/material/Divider'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
@@ -14,7 +12,7 @@ import InboxIcon from '@mui/icons-material/MoveToInbox'
 import MailIcon from '@mui/icons-material/Mail'
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance'
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney'
-import { useNavigate, NavLink } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const drawerWidth = 240
 
@@ -79,19 +77,6 @@ export default function Sidebar(props: Props) {
             ))}
           </List>
           <Divider />
-          {/* 
-					<List>
-						{['All mail', 'Trash', 'Spam'].map((text, index) => (
-							<ListItem key={text} disablePadding>
-								<ListItemButton>
-									<ListItemIcon>
-										{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-									</ListItemIcon>
-									<ListItemText primary={text} />
-								</ListItemButton>
-							</ListItem>
-						))}
-					</List> */}
         </Box>
       </Drawer>
     </Box>
