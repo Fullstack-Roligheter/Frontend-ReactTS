f70cb885ac625cc7a61c6e603ffbcd254ca1dfdb
Mikaela Hedström <mikaela.hedstrom@gmail.com>
2022-09-21 13:18:14 +0200

    MuiFication of dashboard

--- src/features/dashboard/dashboard.tsx
+++ src/features/dashboard/dashboard.tsx
@@ -1,23 +1,24 @@
+import { Box, Typography } from '@mui/material'
 import { Navigate } from 'react-router-dom'
 
 function DashboardFeature() {
-	let UserIsLoggedIn = sessionStorage.getItem('user')
-	console.log('authenticatedLayout: ', UserIsLoggedIn)
+  let UserIsLoggedIn = sessionStorage.getItem('user')
+  console.log('authenticatedLayout: ', UserIsLoggedIn)
 
   let timeMilli = Date.now().toString().slice(-3)
 
-	if (UserIsLoggedIn === null) {
-		return <Navigate to='/login' replace />
-	}
+  if (UserIsLoggedIn === null) {
+    return <Navigate to='/login' replace />
+  }
 
-	return (
-		<div className='Welcome'>
-			<p>DASHBOARD</p>
-			<h1>YOU ARE LOGGED IN</h1>
-			<h2>UserId: {UserIsLoggedIn}</h2>
-      <p>{timeMilli}</p>
-		</div>
-	)
+  return (
+    <Box>
+      <Typography variant='h1'>DASHBOARD</Typography>
+      <Typography variant='h3'>YOU ARE LOGGED IN</Typography>
+      <Typography variant='h5'>UserId: {UserIsLoggedIn}</Typography>
+      <Typography variant='subtitle1'>{timeMilli}</Typography>
+    </Box>
+  )
 }
 
 export default DashboardFeature
