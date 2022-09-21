a967583065a83efaf2e1ac4d0765900641e3a1a1
Mikaela Hedström <mikaela.hedstrom@gmail.com>
2022-09-21 11:03:38 +0200

    More work on welcome

--- src/features/welcome/welcome.tsx
+++ src/features/welcome/welcome.tsx
@@ -8,9 +8,9 @@ function WelcomeFeature() {
         display="flex"
         justifyContent="center"
         alignItems="center"
-        sx={{ paddingTop: '200px', marginTop: '70px' }}
+        sx={{ paddingTop: '150px', marginTop: '70px', }}
       >
-        <Box>
+        <Box sx={{ background: 'radial-gradient(circle at center, rgba(65, 162, 72, 0.6), rgba(65, 162, 72, 0.1))', borderRadius: '15px', paddingLeft: '30px', paddingRight: '30px', paddingTop: '60px', paddingBottom: '60px' }}>
           <Typography variant='h1'>Välkommen att börja spara med Xpense!</Typography>
         </Box>
       </Box>
