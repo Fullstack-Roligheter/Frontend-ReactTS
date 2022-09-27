b682f1ab11a843046c4a3ecefc27a40f4e470fcf
Lachlan Morrison <lachlanmorrison@outlook.com>
2022-09-27 21:13:54 +0200

    småfix fixat GetDebitsForUser fetch justerat forms i ExpenseDashboard

--- src/features/expense/ExpenseDashboard.tsx
+++ src/features/expense/ExpenseDashboard.tsx
@@ -13,9 +13,7 @@ import { GetBudgetsForUser } from '../../shared/fetch/budget'
 import { CreateDebit, GetDebitsForUser } from '../../shared/fetch/expense'
 import { SubmitButton } from '../../shared/buttons/button-default'
 
-
-
-const ExpenseDashboard = (props:userType) => {
+const ExpenseDashboard = (props: userType) => {
   const [categories, setCategories] = useState([])
   const [budgets, setBudgets] = useState([])
   const [debits, setDebits] = useState([])
@@ -30,7 +28,6 @@ const ExpenseDashboard = (props:userType) => {
     // ReturningTransactions: false,
   })
 
-
   console.log(newExpense)
 
   const handleChange = (e: any) => {
@@ -41,112 +38,116 @@ const ExpenseDashboard = (props:userType) => {
     })
   }
 
-  const handleSubmit = (e : any) => {
+  const handleSubmit = (e: any) => {
     e.preventDefault()
-    CreateDebit(newExpense)
-    .then((Response) => {
+    if (newExpense.BudgetId === '') {
+      newExpense.BudgetId = null
+    }
+    if (newExpense.CategoryId === '') {
+      newExpense.CategoryId = null
+    }
+    CreateDebit(newExpense).then((Response) => {
       console.log(Response)
     })
   }
 
   //Get categories for user to put in select
-  useEffect (() => {
-    GetCategoriesForUser(props.userId)
-    .then((Response) => {
+  useEffect(() => {
+    GetCategoriesForUser(props.userId).then((Response) => {
       console.log(Response)
       setCategories(Response)
     })
-  },[])
+  }, [])
 
   //Get budgets for user to put in select
-  useEffect (() => {
-    GetBudgetsForUser(props.userId)
-    .then((Response) => {
+  useEffect(() => {
+    GetBudgetsForUser(props.userId).then((Response) => {
       console.log(Response)
       setBudgets(Response)
     })
-  },[])
+  }, [])
 
   //Get all debits to put in list
-  useEffect (() => {
-    GetDebitsForUser(props.userId)
-    .then((Response) => {
+  useEffect(() => {
+    console.log('props.userId: ', props.userId)
+    GetDebitsForUser(props.userId).then((Response) => {
       console.log(Response)
       setDebits(Response)
     })
-  },[])
+  }, [])
   console.log(debits)
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-          <TextField
-            required
-            //   label='Date'
-            type='date'
-            name='Date'
-            value={newExpense.Date}
-            onChange={handleChange}
-          />
-          <TextField
-            required
-            label='Amount'
-            type='number'
-            name='Amount'
-            value={newExpense.Amount}
-            onChange={handleChange}
-            InputProps={{
-              endAdornment: <InputAdornment position='end'>Kr</InputAdornment>,
-            }}
-          />
-          <TextField
-            required
-            select
-            label='Category'
-            name='CategoryId'
-            value={''}
-            onChange={handleChange}
-            SelectProps={{
-              native: true,
-            }}
-          >
-            {categories.map((option:any) => (
-              <option key={option.categoryId} value={option.categoryId}>
-                {option.categoryName}
-              </option>
-            ))}
-          </TextField>
-          <TextField
-            select
-            label='Budget'
-            name='BudgetId'
-            value={newExpense.BudgetId}
-            onChange={handleChange}
-            SelectProps={{
-              native: true,
-            }}
-          >
-            {budgets.map((option:any) => (
-              <option key={option.budgetId} value={option.budgetId}>
-                {option.budgetName}
-              </option>
-            ))}
-          </TextField>
-          <TextField
-            label='Description'
-            multiline
-            rows={5}
-            helperText='Company, Notes, Reciever Etc.'
-            name='Comment'
-            value={newExpense.Comment}
-            onChange={handleChange}
-          />
-          {/* <Checkbox
+        <TextField
+          required
+          //   label='Date'
+          type='date'
+          name='Date'
+          value={newExpense.Date}
+          onChange={handleChange}
+        />
+        <TextField
+          required
+          label='Amount'
+          type='number'
+          name='Amount'
+          value={newExpense.Amount}
+          onChange={handleChange}
+          InputProps={{
+            endAdornment: <InputAdornment position='end'>Kr</InputAdornment>,
+          }}
+        />
+        <TextField
+          select
+          label='Category'
+          name='CategoryId'
+          value={newExpense.CategoryId}
+          onChange={handleChange}
+          SelectProps={{
+            native: true,
+          }}
+        >
+          <option value='' />
+          {categories.map((option: any) => (
+            <option key={option.categoryId} value={option.categoryId}>
+              {option.categoryName}
+            </option>
+          ))}
+        </TextField>
+        <TextField
+          select
+          label='Budget'
+          name='BudgetId'
+          value={newExpense.BudgetId}
+          onChange={handleChange}
+          SelectProps={{
+            native: true,
+          }}
+        >
+          <option value='' />
+          {budgets.map((option: any) => (
+            <option key={option.budgetId} value={option.budgetId}>
+              {option.budgetName}
+            </option>
+          ))}
+        </TextField>
+        <TextField
+          label='Description'
+          multiline
+          rows={5}
+          helperText='Company, Notes, Reciever Etc.'
+          name='Comment'
+          value={newExpense.Comment}
+          onChange={handleChange}
+        />
+        {/* <Checkbox
           aria-label='Returning transactions'
           sx={{ '& .MuiSvgIcon-root': { fontSize: 28 } }}
         /> */}
 
-        <SubmitButton type='submit' onClick={handleSubmit}/>
+        <SubmitButton type='submit' onClick={handleSubmit} />
       </form>
     </>
   )
