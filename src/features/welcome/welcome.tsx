d19d1113aabe5a0cb1216839bd5f2d6c4a40a41a
MikaelaNystrom <mikaela.hedstrom@gmail.com>
2022-10-03 19:46:53 +0200

    Rename Rename to welcomeBackgroundColor

--- src/features/welcome/welcome.tsx
+++ src/features/welcome/welcome.tsx
@@ -1,19 +1,6 @@
 import { Box, Typography } from "@mui/material"
 import styles from '../../styles.js'
 
-// const styles = {
-//   color: {
-//     background: 'radial-gradient(circle at center, rgba(65, 162, 72, 0.4), rgba(65, 162, 72, 0.1))',
-//     borderRadius: '15px',
-//     paddingLeft: '30px',
-//     paddingRight: '30px',
-//     paddingTop: '60px',
-//     paddingBottom: '60px',
-//     display: 'flex',
-//     justifyItems: 'center',
-//   },
-// };
-
 function WelcomeFeature() {
   return (
     <>
@@ -23,7 +10,7 @@ function WelcomeFeature() {
         alignItems="center"
         sx={{ paddingTop: '100px', marginTop: '70px', }}
       >
-        <Box style={styles.color}>
+        <Box style={styles.welcomeBackgroundColor}>
           <Typography variant='h1' color="white" align="center" sx={{ textShadow: '1px 1px 2px black' }}>Välkommen att börja spara med Xpense!</Typography>
         </Box>
       </Box>
--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,5 @@
 const styles = {
-  color: {
+  welcomeBackgroundColor: {
     background: 'radial-gradient(circle at center, rgba(65, 162, 72, 0.4), rgba(65, 162, 72, 0.1))',
     borderRadius: '15px',
     paddingLeft: '30px',
