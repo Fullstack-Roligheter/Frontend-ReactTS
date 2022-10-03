2335638ce90e369cdd9ea54e12ae414c0a97de3d
MikaelaNystrom <mikaela.hedstrom@gmail.com>
2022-10-03 20:41:07 +0200

    FaqUsesStyleFile

--- src/features/faq/faq.tsx
+++ src/features/faq/faq.tsx
@@ -1,14 +1,16 @@
 import { Box, Typography } from "@mui/material"
+import styles from '../../styles.js'
+
 
 const Faq = () => {
   return (
     <>
       <Box sx={{ marginTop: '70px' }}>
         <Box>
-          <Typography variant='h1'>Faq</Typography>
+          <Typography variant='h1' style={styles.whiteTypography}>Faq</Typography>
         </Box>
         <Box>
-          <Typography variant='h5'>Woop Woop</Typography>
+          <Typography variant='h5' style={styles.whiteTypography}>Woop Woop</Typography>
         </Box>
       </Box>
     </>
