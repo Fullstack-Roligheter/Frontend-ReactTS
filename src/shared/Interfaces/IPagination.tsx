74fad9a2916c7e15505af8074d6a10eee33e2a5b
Daniela Escobar <daniela.b.escobar@gmail.com>
2022-10-20 15:48:28 +0200

    Add more types to interface IPagination

--- src/shared/Interfaces/IPagination.tsx
+++ src/shared/Interfaces/IPagination.tsx
@@ -1,4 +1,12 @@
+import { ChangeEvent, ReactEventHandler } from "react"
+
 export interface IPagination {
  data: any,
- itemsPerPage: number
+ itemsPerPage: number,
+ value: any,
+ event: React.ChangeEvent<HTMLInputElement>,
+ setCurrentPage: number,
+ pageCount: number,
+ onPageChange: any,
+currentPage: number
 }
