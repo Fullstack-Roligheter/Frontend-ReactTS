d72a12d2be43cb81a6868ee357aaa2117701f658
Bergewing <s.bergewing@hotmail.com>
2022-10-26 20:11:36 +0200

    Ej fungerande commit

--- src/features/expense/ExpenseListOutput.tsx
+++ src/features/expense/ExpenseListOutput.tsx
@@ -9,10 +9,14 @@ import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 import { useUserContext } from '../../context/UserContext'
+import { Collapse, IconButton } from '@mui/material'
+import { KeyboardArrowUp, KeyboardArrowDown } from '@mui/icons-material'
 
 const ExpenseListOutput = () => {
   const user = useUserContext()
+
   const [debits, setDebits] = useState<any[]>([])
+  const [open, setOpen] = useState(false)
 
   //Get all debits to put in list
   useEffect(() => {
@@ -24,15 +28,58 @@ const ExpenseListOutput = () => {
 
   return (
     <>
+      {/* <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+          </IconButton>
+        </TableCell>
+        <TableCell align="right">Datum</TableCell>
+        <TableCell align="right">Summa</TableCell>
+        <TableCell align="right">Kategori</TableCell>
+        <TableCell align="right">Budget</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Customer</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                    <TableCell align="right">Total price ($)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {debits.map((debit) => (
+                    <TableRow key={debit.id}>
+                      <TableCell component="th" scope="row">
+                        {debit.date}
+                      </TableCell>
+                      <TableCell>{debit.customerId}</TableCell>
+                      <TableCell align="right">{debit.amount}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+          </Collapse>
+        </TableCell>
+      </TableRow> */}
+
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label='simple table'>
           <TableHead>
+            <TableCell></TableCell>
             <TableRow>
               <TableCell>Datum</TableCell>
               <TableCell>Summa</TableCell>
               <TableCell>Kategori</TableCell>
               <TableCell>Budget</TableCell>
-              <TableCell>Kommentar</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -41,11 +88,24 @@ const ExpenseListOutput = () => {
                 key={debit.id}
                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
               >
+                <IconButton
+                  aria-label='expand row'
+                  size='small'
+                  onClick={() => setOpen(!open)}
+                >
+                  {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+                </IconButton>
                 <TableCell>{debit.date}</TableCell>
                 <TableCell>{debit.amount}</TableCell>
                 <TableCell>{debit.category}</TableCell>
                 <TableCell>{debit.budget}</TableCell>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                <TableRow>
+                  <TableCell>Kommentar</TableCell>
+
+                </TableRow>
                 <TableCell>{debit.comment}</TableCell>
+                </Collapse>
               </TableRow>
             ))}
           </TableBody>
