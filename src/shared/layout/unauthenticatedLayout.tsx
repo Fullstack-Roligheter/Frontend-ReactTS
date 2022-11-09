160719fc3877f5ce635e999a2c4ebb3217ed224d
Lachlan Morrison <lachlanmorrison@outlook.com>
2022-11-09 20:20:24 +0100

    ändrat unauthenticatedHeader också

--- src/shared/layout/unauthenticatedLayout.tsx
+++ src/shared/layout/unauthenticatedLayout.tsx
@@ -6,7 +6,7 @@ import { Box } from '@mui/material'
 const UnauthenticatedLayout = (props: any) => {
   return (
     <>
-      <Box height='100vh'>
+      <Box height='calc(100vh - 56px)'>
         <Header />
         <Box>{props.children}</Box>
         <Outlet />
